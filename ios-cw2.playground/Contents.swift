494a4c232553a8e77e4564400b296bb43516b279
Fatmah-alrashidi-C55 <66441802+Fatmah-alrashidi-481@users.noreply.github.com>
2020-09-12 23:36:10 +0300

    done

---
+++ ios-cw2.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var str = "Hello, playground"
+
+let famliy = ["abdullah" ,"fatmah" , "omar" , "othman" , "almarmar" ]
+var evens = [2 , 4 , 6 , 8 , 10 , 12 ]
+var grades = [3.1 , 3.5 , 3.9 ]
+var turefalse = ["ture" , "ture" , "ture" , "false"]
+
+print ( "famliy:", famliy[0] ,famliy [4])
+print( "evens:" , evens[0], evens[5])
+print( "grades:" , grades[0], grades[2])
+print( "turefalse:" , turefalse[0], turefalse[3])
